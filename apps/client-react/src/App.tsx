bcd605520a5c65b6bb72fac95c06023df3a417d7
Sebastian F <sebpfdev@gmail.com>
2024-03-24 19:57:27 -0400

    dev: stub out app routes

--- apps/client-react/src/App.tsx
+++ apps/client-react/src/App.tsx
@@ -1,3 +1,18 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		errorElement: <div>Error</div>,
+		children: [
+			{ index: true, element: <div>RootIndex</div> },
+			{ path: "/projects/create", element: <div>Project create</div> },
+			{ path: "/projects/:id", element: <div>Project Id</div> },
+			{ path: "/about", element: <div>About</div> },
+		],
+	},
+]);
+
 export default function App() {
-	return <div>App</div>;
+	return <RouterProvider router={router} />;
 }
