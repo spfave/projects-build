61c72cfbda349cb14b43d0b86a5d5f36ff361bb8
Sebastian F <sebpfdev@gmail.com>
2024-05-06 21:57:58 -0400

    Update projects route UI: create sidebar nav links

--- apps/client-react/src/views/projects-route.tsx
+++ apps/client-react/src/views/projects-route.tsx
@@ -1,22 +1,40 @@
-import React from "react";
+import * as React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 import type { Project } from "@projectsbuild/types";
 
 import styles from "./projects-route.module.css";
 
+async function getProjects() {
+	const res = await fetch(`${import.meta.env.VITE_API_URL_JSON_SERVER}/projects`);
+
+	// if (!res.ok) throw new Error("Fetch to Get Projects failed");
+	// if (res.status)
+
+	return res.json();
+}
+
 export default function ProjectsRoute() {
 	const [projects, setProjects] = React.useState<Project[]>();
 
 	React.useEffect(() => {
-		fetch(`${import.meta.env.VITE_API_URL_JSON_SERVER}/projects`)
-			.then((res) => res.json())
-			.then((data: Project[]) => setProjects(data));
+		getProjects().then((data) => setProjects(data));
 	}, []);
 
 	return (
-		<div className={styles.projectsContent}>
-			Projects Grid
-			{JSON.stringify(projects)}
+		<div className={styles.projects}>
+			<div className={styles.projectsSidebar}>
+				<nav>
+					{projects?.map((project) => (
+						<Link key={project.id} to={project.id.toString()}>
+							{project.name}
+						</Link>
+					))}
+				</nav>
+			</div>
+			<div className={styles.projectsOutlet}>
+				<Outlet />
+			</div>
 		</div>
 	);
 }
