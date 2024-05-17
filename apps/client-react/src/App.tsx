4d37fb614679b0757be3f273ee935f21b07e7da9
Sebastian F <sebpfdev@gmail.com>
2024-05-16 20:54:51 -0400

    Use react router future flags

--- apps/client-react/src/App.tsx
+++ apps/client-react/src/App.tsx
@@ -3,27 +3,37 @@ import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom"
 import ProjectsRoute from "~/views/projects-route";
 import Root from "~/views/root";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <Root />,
+			errorElement: <div>Error</div>,
+			children: [
+				{ index: true, element: <Navigate to="projects" /> },
+				{
+					path: "projects",
+					element: <ProjectsRoute />,
+					children: [
+						{ path: "create", element: <div>Project Create</div> },
+						{ path: ":id", element: <div>Project Id</div> },
+						{ path: ":id/edit", element: <div>Project Id Edit</div> },
+					],
+				},
+				{ path: "about", element: <div>About</div> },
+			],
+		},
+	],
 	{
-		path: "/",
-		element: <Root />,
-		errorElement: <div>Error</div>,
-		children: [
-			{ index: true, element: <Navigate to="projects" /> },
-			{
-				path: "projects",
-				element: <ProjectsRoute />,
-				children: [
-					{ path: "create", element: <div>Project Create</div> },
-					{ path: ":id", element: <div>Project Id</div> },
-					{ path: ":id/edit", element: <div>Project Id Edit</div> },
-				],
-			},
-			{ path: "about", element: <div>About</div> },
-		],
-	},
-]);
+		future: {
+			v7_fetcherPersist: true,
+			v7_normalizeFormMethod: true,
+			v7_relativeSplatPath: true,
+			v7_partialHydration: true,
+		},
+	}
+);
 
 export default function App() {
-	return <RouterProvider router={router} />;
+	return <RouterProvider router={router} future={{ v7_startTransition: true }} />;
 }
--- apps/client-react/vite.config.ts
+++ apps/client-react/vite.config.ts
@@ -5,9 +5,10 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: { port: 3001 },
 	resolve: {
 		alias: { "~": path.resolve(__dirname, "./src") },
 	},
+
+	server: { port: 3001 },
 	envDir: "../../",
 });
