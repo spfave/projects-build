607bc1accf769cfb510113026efea7c179d1b6d0
Sebastian F <sebpfdev@gmail.com>
2024-05-16 22:02:58 -0400

    Update projects route component

--- apps/client-react/src/views/projects-route.tsx
+++ apps/client-react/src/views/projects-route.tsx
@@ -7,18 +7,19 @@ import styles from "./projects-route.module.css";
 
 async function getProjects() {
 	const res = await fetch(`${import.meta.env.VITE_URL_API_JSON_SERVER}/projects`);
+	// if (!res.ok) throw new FetchError("Fetch to Get Projects failed", { cause: res });
+	// if (res.status !== 200) ...
 
-	// if (!res.ok) throw new Error("Fetch to Get Projects failed");
-	// if (res.status)
-
-	return res.json();
+	const projects = (await res.json()) as Project[];
+	return projects;
 }
 
 export default function ProjectsRoute() {
 	const [projects, setProjects] = React.useState<Project[] | null>(null);
 
 	React.useEffect(() => {
-		getProjects().then((data) => setProjects(data));
+		setProjects(null);
+		getProjects().then(setProjects);
 	}, []);
 
 	if (projects == null) return <div>Loading Projects...</div>;
@@ -29,7 +30,7 @@ export default function ProjectsRoute() {
 		<div className={styles.projects}>
 			<div className={styles.projectsSidebar}>
 				<nav>
-					{projects?.map((project) => (
+					{projects.map((project) => (
 						<Link key={project.id} to={project.id.toString()}>
 							{project.name}
 						</Link>
