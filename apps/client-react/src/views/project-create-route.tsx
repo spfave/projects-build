6af4fce8f99cef97118b894dbca0a29d67fcf98d
Sebastian F <sebpfdev@gmail.com>
2024-05-18 22:05:36 -0400

    Conditionally show form fields

--- apps/client-react/src/views/project-create-route.tsx
+++ apps/client-react/src/views/project-create-route.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 
+import type { ProjectStatus } from "@projectsbuild/types";
+
 import styles from "./project-create-route.module.css";
 
 export default function ProjectCreateRoute() {
+	const [projectStatus, setProjectStatus] = React.useState<ProjectStatus>();
+
+	function handleChangeStatus(evt: React.ChangeEvent<HTMLSelectElement>) {
+		setProjectStatus(evt.target.value as ProjectStatus);
+	}
+
 	return (
 		<div className={styles.projectCreate}>
 			<h2>Define New Project</h2>
@@ -35,42 +43,63 @@ export default function ProjectCreateRoute() {
 					<label htmlFor="status" className="block">
 						Status
 					</label>
-					<select id="status" name="status">
-						<option value="">Select a Status</option>
+					<select
+						id="status"
+						name="status"
+						defaultValue="select"
+						value={projectStatus}
+						onChange={handleChangeStatus}
+					>
+						<option value="select" hidden>
+							Select a Status
+						</option>
 						<option value="planning">Planning</option>
 						<option value="building">Building</option>
 						<option value="complete">Complete</option>
 					</select>
 				</div>
 
-				{/* Conditional */}
-				<div className="form-field">
-					<label htmlFor="dateCompleted" className="block">
-						Date completed
-					</label>
-					<input id="dateCompleted" type="date" name="dateCompleted" />
-				</div>
-				<div className="form-field">
-					<label htmlFor="rating" className="block">
-						Rating
-					</label>
-					<input id="rating" type="number" name="rating" min={1} max={5} />
-				</div>
-				<div className="form-field">
-					<fieldset>
-						<legend>Would you recommend</legend>
-						<div>
-							<span>
-								<input id="recommend-yes" type="radio" value="true" name="recommend" />
-								<label htmlFor="recommend-yes">Yes</label>
-							</span>
-							<span>
-								<input id="recommend-no" type="radio" value="false" name="recommend" />
-								<label htmlFor="recommend-no">No</label>
-							</span>
+				{projectStatus === "complete" && (
+					<>
+						<div className="form-field">
+							<label htmlFor="dateCompleted" className="block">
+								Date completed
+							</label>
+							<input id="dateCompleted" type="date" name="dateCompleted" />
 						</div>
-					</fieldset>
-				</div>
+						<div className="form-field">
+							<label htmlFor="rating" className="block">
+								Rating
+							</label>
+							<input id="rating" type="number" name="rating" min={1} max={5} />
+						</div>
+						<div className="form-field">
+							<fieldset>
+								<legend>Would you recommend</legend>
+								<div>
+									<span>
+										<input
+											id="recommend-yes"
+											type="radio"
+											value="true"
+											name="recommend"
+										/>
+										<label htmlFor="recommend-yes">Yes</label>
+									</span>
+									<span>
+										<input
+											id="recommend-no"
+											type="radio"
+											value="false"
+											name="recommend"
+										/>
+										<label htmlFor="recommend-no">No</label>
+									</span>
+								</div>
+							</fieldset>
+						</div>
+					</>
+				)}
 
 				<div>
 					<button type="submit">Create Project</button>
