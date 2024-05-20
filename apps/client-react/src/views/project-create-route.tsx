e48c260488b29624b275796d507603b8682680af
Sebastian F <sebpfdev@gmail.com>
2024-05-20 18:54:55 -0400

    Enable creating a project

--- apps/client-react/src/views/project-create-route.tsx
+++ apps/client-react/src/views/project-create-route.tsx
@@ -1,8 +1,18 @@
 import * as React from "react";
 
-import type { ProjectStatus } from "@projectsbuild/types";
+import type { ProjectInput, ProjectStatus } from "@projectsbuild/types";
 
 import styles from "./project-create-route.module.css";
+
+async function createProject(project: ProjectInput) {
+	const res = await fetch(`${import.meta.env.VITE_URL_API_JSON_SERVER}/projects`, {
+		method: "POST",
+		body: JSON.stringify(project),
+	});
+	const data = await res.json();
+
+	return;
+}
 
 export default function ProjectCreateRoute() {
 	const [projectStatus, setProjectStatus] = React.useState<ProjectStatus>();
@@ -15,14 +25,15 @@ export default function ProjectCreateRoute() {
 		evt.preventDefault();
 
 		const formData = new FormData(evt.currentTarget);
-		const formObj = Object.fromEntries(formData.entries());
-		console.info(`formObj: `, formObj); //LOG
+		const formObj = Object.fromEntries(formData.entries()) as ProjectInput;
+
+		createProject(formObj);
 	}
 
 	return (
 		<div className={styles.projectCreate}>
 			<h2>Define New Project</h2>
-			<form onSubmit={handelCreateProject}>
+			<form method="POST" action="./" onSubmit={handelCreateProject}>
 				<div className={styles.flexFormGroup}>
 					<div>
 						<label htmlFor="name" className="block">
