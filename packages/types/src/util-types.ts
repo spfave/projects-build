f2b39cf0fbfb0335d3478e545dabcd5be06938bf
Sebastian F <sebpfdev@gmail.com>
2024-06-18 19:18:24 -0400

    Create type utilities

--- packages/types/src/util-types.ts
+++ packages/types/src/util-types.ts
@@ -1,2 +1,9 @@
+// biome-ignore lint/complexity/noBannedTypes: create non-nullish type alias
+export type NonNullish = {};
+
+export type Prettify<T> = {
+	[K in keyof T]: T[K];
+} & unknown;
+
 // export type DateString = string;
-// export type DateString = `${string}-${string}-${string}`;
+// export type DateYMD = `${string}-${string}-${string}`;
