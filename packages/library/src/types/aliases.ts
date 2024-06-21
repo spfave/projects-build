b0e5f43a3638b9c8f8a3e53eeec89770eccdcf51
Sebastian F <sebpfdev@gmail.com>
2024-06-21 13:48:33 -0400

    Move aliases and helper types to library package

---
+++ packages/library/src/types/aliases.ts
@@ -0,0 +1,5 @@
+// biome-ignore lint/complexity/noBannedTypes: create non-nullish type alias
+export type NonNullish = {};
+
+// export type DateString = string;
+// export type DateYMD = `${string}-${string}-${string}`;
---
+++ packages/library/src/types/helpers.ts
@@ -0,0 +1,6 @@
+declare const brand: unique symbol;
+export type Brand<T, TBrand> = T & { [brand]: TBrand };
+
+export type Prettify<T> = {
+	[K in keyof T]: T[K];
+} & {};
