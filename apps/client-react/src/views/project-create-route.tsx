2bbdd9b41c04a40a1746fcfd752ba9b5769360e3
Sebastian F <sebpfdev@gmail.com>
2024-07-01 20:32:10 -0400

    Create project validation function

--- apps/client-react/src/views/project-create-route.tsx
+++ apps/client-react/src/views/project-create-route.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
-import { valueIfTruthy, ymdToday } from "@projectsbuild/library/utils";
+import { isValidYMD, isYMD, valueIfTruthy, ymdToday } from "@projectsbuild/library/utils";
+import { isStringParsableInt } from "@projectsbuild/library/validation";
 import type {
 	Project,
 	ProjectForm,
@@ -20,6 +21,113 @@ export async function createProject(project: ProjectInput) {
 	const newProject = (await res.json()) as Project;
 
 	return newProject;
+}
+
+export type ProjectErrors = {
+	formErrors: string[];
+	fieldErrors: {
+		id: string[];
+		name: string[];
+		link: string[];
+		description: string[];
+		notes: string[];
+		status: string[];
+		dateCompleted: string[];
+		rating: string[];
+		recommend: string[];
+	};
+};
+export function validateProject(input: Record<string, FormDataEntryValue>) {
+	const errors: ProjectErrors = {
+		formErrors: [],
+		fieldErrors: {
+			id: [],
+			name: [],
+			link: [],
+			description: [],
+			notes: [],
+			status: [],
+			dateCompleted: [],
+			rating: [],
+			recommend: [],
+		},
+	};
+
+	// validate input is an object
+	if (!input || Array.isArray(input) || typeof input !== "object") {
+		errors.formErrors.push("Invalid project input");
+		return { status: "error", errors } as const;
+	}
+
+	const { name, link, description, notes, status, dateCompleted, rating, recommend } =
+		input;
+
+	// validate fields
+	// name
+	if (name == null) errors.fieldErrors.name.push("Name must be provided");
+	else if (typeof name !== "string")
+		errors.fieldErrors.name.push("Name must be a string");
+	else if (name.trim().length < 2)
+		errors.fieldErrors.name.push("Name must be at least 2 characters");
+
+	// link
+	if (link && typeof link !== "string")
+		errors.fieldErrors.link.push("Link must be a string");
+
+	// description
+	if (description && typeof description !== "string")
+		errors.fieldErrors.description.push("Description must be a string");
+
+	// notes
+	if (notes && typeof notes !== "string")
+		errors.fieldErrors.notes.push("Notes must be a string");
+
+	// status
+	if (status == null) errors.fieldErrors.status.push("Status must be provided");
+	else if (
+		typeof status !== "string" ||
+		!["planning", "building", "complete"].includes(status)
+	) {
+		errors.fieldErrors.status.push("Invalid status");
+	}
+
+	if (status === "complete") {
+		// dateCompleted
+		if (dateCompleted == null)
+			errors.fieldErrors.dateCompleted.push("Completion date must be provided");
+		else if (typeof dateCompleted !== "string")
+			errors.fieldErrors.dateCompleted.push("Invalid date type");
+		else if (!isYMD(dateCompleted))
+			errors.fieldErrors.dateCompleted.push("Invalid format. Use YYYY-MM-DD");
+		else if (!isValidYMD(dateCompleted) || dateCompleted > ymdToday())
+			errors.fieldErrors.dateCompleted.push("Invalid date");
+
+		// rating
+		if (rating == null) errors.fieldErrors.rating.push("Rating must be provided");
+		else if (
+			// biome-ignore format: maintain condition checks on single line
+			(typeof rating === "string" && (!isStringParsableInt(rating) || +rating < 1 || +rating > 5)) ||
+			(typeof rating === "number" && (!Number.isInteger(rating) || rating < 0 || rating > 5))
+		) {
+			errors.fieldErrors.rating.push("Rating must be a whole number 1 through 5");
+		}
+
+		// recommend
+		if (recommend == null)
+			errors.fieldErrors.recommend.push("Recommendation must be provided");
+		else if (
+			!["boolean", "string"].includes(typeof recommend) ||
+			(typeof recommend === "string" && !["true", "false"].includes(recommend))
+		)
+			errors.fieldErrors.recommend.push("Invalid recommendation");
+	}
+
+	const hasErrors =
+		errors.formErrors.length ||
+		Object.values(errors.fieldErrors).some((fieldErrors) => fieldErrors.length);
+
+	if (hasErrors) return { status: "error", errors } as const;
+	return undefined;
 }
 
 export function transformProject(
@@ -59,6 +167,7 @@ export default function ProjectCreateRoute() {
 
 		const formData = new FormData(evt.currentTarget);
 		const formObj = Object.fromEntries(formData.entries());
+		const validation = validateProject(formObj);
 
 		const projectPayload = transformProject(formObj as ProjectForm);
 		const project = await createProject(projectPayload);
