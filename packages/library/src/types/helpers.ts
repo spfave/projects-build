d0fc15fb983d86a1f0ab38eb3244319841ccc949
Sebastian F <sebpfdev@gmail.com>
2024-07-10 23:44:35 -0400

    Formatting changes

--- packages/library/src/types/helpers.ts
+++ packages/library/src/types/helpers.ts
@@ -1,6 +1,4 @@
 declare const brand: unique symbol;
 export type Brand<T, TBrand> = T & { [brand]: TBrand };
 
-export type Prettify<T> = {
-	[K in keyof T]: T[K];
-} & {};
+export type Prettify<T> = { [K in keyof T]: T[K] } & {};
