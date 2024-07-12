5b6ecf3d5c13e1fc1306ce53c03fe7decdb7cf3f
Sebastian F <sebpfdev@gmail.com>
2024-07-12 17:56:30 -0400

    Define general isString regex validation helper

--- packages/library/src/validation/regex-validators.ts
+++ packages/library/src/validation/regex-validators.ts
@@ -3,6 +3,10 @@ export const REGEX = Object.freeze({
 	DIGITS_ONLY_NON_LEADING_ZERO: /^[1-9]{1}\d*$/,
 });
 
+export function isString(value: string, regex: keyof typeof REGEX): boolean {
+	return REGEX[regex].test(value);
+}
+
 export function isDigitsOnlyNonLeadingZero(value: string): boolean {
 	return REGEX.DIGITS_ONLY_NON_LEADING_ZERO.test(value);
 }
