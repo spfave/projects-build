76ca7338c59580f22a8eab0fc430ea419afde170
Sebastian F <sebpfdev@gmail.com>
2024-07-23 18:46:16 -0400

    Update helper type comments

--- packages/library/src/types/helpers.ts
+++ packages/library/src/types/helpers.ts
@@ -9,19 +9,19 @@ declare const brand: unique symbol;
 export type Brand<T, TBrand> = T & { [brand]: TBrand };
 
 /**
- * Construct a type with the properties of TObj, converting non-string property values
- * to string as provided by a HTML form field input types.
+ * Construct a type with the properties of TForm, converts non-string property values
+ * to string as provided by HTML form field elements.
  */
 export type FormFields<TForm extends RecordStr> = {
 	[Field in keyof TForm]: TForm[Field] extends string ? TForm[Field] : string;
 };
 
-/** Construct a type with form and form field error lists */
+/** Construct a type for TForm with form and form field element error lists. */
 export type FormErrors<TForm extends RecordStr> = {
 	form: string[];
 	fields: { [Field in keyof TForm]-?: string[] };
 };
 
-export type Maybe<T> = T | undefined | null;
+export type Maybe<T> = T | null | undefined;
 
 export type Prettify<T> = { [K in keyof T]: T[K] } & {};
