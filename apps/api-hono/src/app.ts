f32ed85b29944a4e88706306112fc5e2c8bed746
Sebastian F <sebpfdev@gmail.com>
2024-11-02 23:33:15 -0400

    Extract project api routes

--- apps/api-hono/src/app.ts
+++ apps/api-hono/src/app.ts
@@ -1,31 +1,13 @@
 import { Hono } from "hono";
 
+import projects from "#routes/projects.ts";
+
 const app = new Hono({ strict: false });
 
-app.get("/projects", (ctx) => {
-	return ctx.json({ message: "all projects" });
-});
-
-app.get("/projects/:id", (ctx) => {
-	return ctx.json({ message: `get project with id: ${ctx.req.param("id")}` });
-});
-
-app.post("/projects", (ctx) => {
-	return ctx.json({ message: "create project" });
-});
-
-app.patch("/projects/:id", (ctx) => {
-	return ctx.json({ message: `update project with id: ${ctx.req.param("id")}` });
-});
-
-app.delete("/projects/:id", (ctx) => {
-	return ctx.json({ message: `delete project with id: ${ctx.req.param("id")}` });
-});
-
+app.route("/", projects);
 app.notFound((ctx) => {
 	return ctx.json({ message: `Not Found - ${ctx.req.url}` }, 404);
 });
-
 app.onError((error, ctx) => {
 	return ctx.json({ message: error.message, stack: error.stack }, 500);
 });
---
+++ apps/api-hono/src/routes/projects.ts
@@ -0,0 +1,26 @@
+import { Hono } from "hono";
+
+const api = new Hono().basePath("/projects");
+
+api.get("/", (ctx) => {
+	return ctx.json({ message: "all projects" });
+});
+
+api.get("/:id", (ctx) => {
+	return ctx.json({ message: `get project with id: ${ctx.req.param("id")}` });
+});
+
+api.post("/", (ctx) => {
+	return ctx.json({ message: "create project" });
+});
+
+api.patch("/:id", (ctx) => {
+	return ctx.json({ message: `update project with id: ${ctx.req.param("id")}` });
+});
+
+api.delete("/:id", (ctx) => {
+	return ctx.json({ message: `delete project with id: ${ctx.req.param("id")}` });
+});
+
+export default api;
+// export { api };
