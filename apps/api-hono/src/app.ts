fdd673dbbb2180b41fa2e3803427b157a0a4d6ee
Sebastian F <sebpfdev@gmail.com>
2024-11-04 21:46:04 -0500

    Use hono logger middleware

--- apps/api-hono/src/app.ts
+++ apps/api-hono/src/app.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 import projects from "#routes/projects.ts";
 import { notFound, onError } from "#utils/route.utils.ts";
 
 const app = new Hono({ strict: false }).basePath("/api");
+
+app.use(logger());
 
 app.route("/", projects);
 app.notFound(notFound);
