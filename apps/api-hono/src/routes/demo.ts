711336920bdfdb563a4fe2e80525e5539e341d76
Sebastian F <sebpfdev@gmail.com>
2024-11-04 22:06:14 -0500

    Add bearer auth demo endpoint

--- apps/api-hono/src/routes/demo.ts
+++ apps/api-hono/src/routes/demo.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { basicAuth } from "hono/basic-auth";
+import { bearerAuth } from "hono/bearer-auth";
 import { HTTPException } from "hono/http-exception";
 
 const api = new Hono().basePath("/demo");
@@ -7,16 +8,34 @@ const api = new Hono().basePath("/demo");
 api.use(
 	"/auth-basic",
 	basicAuth({
-		username: "seabass",
-		password: "demo-apis",
+		username: "demo-auth-usrn",
+		password: "demo-auth-pswd",
 		invalidUserMessage: (ctx) => {
-			throw new HTTPException(401, { message: "Not Authenticated" });
+			throw new HTTPException(401, {
+				message: "Not Authenticated - Invalid Credentials",
+			});
 		},
 	})
 );
 api.get("/auth-basic", (ctx) => {
 	console.log(ctx.newResponse(null).status);
 	return ctx.json({ message: "Authenticated - Basically" }, 200);
+});
+
+api.use(
+	"/auth-bearer",
+	bearerAuth({
+		token: "demo-auth-tkn",
+		invalidTokenMessage: (ctx) => {
+			throw new HTTPException(401, {
+				message: "Not Authenticated - Invalid Bearer Token",
+			});
+		},
+	})
+);
+api.get("/auth-bearer", (ctx) => {
+	console.log(ctx.newResponse(null).status);
+	return ctx.json({ message: "Authenticated - Bearer" }, 200);
 });
 
 export default api;
