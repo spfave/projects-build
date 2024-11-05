2d3419dc60536172b919b4bd3beaffa4cb5acc8b
Sebastian F <sebpfdev@gmail.com>
2024-11-04 22:42:06 -0500

    Add restrict-ip and request-id demo endpoints

--- apps/api-hono/src/routes/demo.ts
+++ apps/api-hono/src/routes/demo.ts
@@ -1,7 +1,10 @@
+import { getConnInfo } from "@hono/node-server/conninfo";
 import { Hono } from "hono";
 import { basicAuth } from "hono/basic-auth";
 import { bearerAuth } from "hono/bearer-auth";
 import { HTTPException } from "hono/http-exception";
+import { ipRestriction } from "hono/ip-restriction";
+import { requestId } from "hono/request-id";
 
 const api = new Hono().basePath("/demo");
 
@@ -37,5 +40,34 @@ api.get("/auth-bearer", (ctx) => {
 	console.log(ctx.newResponse(null).status);
 	return ctx.json({ message: "Authenticated - Bearer" }, 200);
 });
+
+api.use(
+	"/restrict-ip",
+	ipRestriction(
+		getConnInfo,
+		{
+			denyList: ["127.0.0.1"],
+			allowList: [],
+		},
+		(remote, ctx) => {
+			return ctx.json(
+				{ message: `Blocking access from ${remote.type} ${remote.addr}` },
+				403
+			);
+		}
+	)
+);
+api.get("/restrict-ip", (ctx) => {
+	return ctx.json({ message: "Your ip made it through", ...getConnInfo(ctx) }, 200);
+});
+
+api.use("/request-id", requestId()).get((ctx) => {
+	console.debug("requestId:", ctx.var.requestId);
+	return ctx.json({ message: `Request id is ${ctx.get("requestId")}` }, 200);
+});
+
+// api.get("timeout", (ctx) => {});
+
+// api.get("rate-limiter", (ctx) => {});
 
 export default api;
