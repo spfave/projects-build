38b6d32263e0e5f44e94434cbdbcfd2ca8dc861f
Sebastian F <sebpfdev@gmail.com>
2024-12-31 08:28:47 -0500

    Move dev query sample calls

--- packages/db-drizzle/src/db/db.ts
+++ packages/db-drizzle/src/db/db.ts
@@ -9,24 +9,3 @@ export const db = drizzle({
 	schema,
 	logger: process.env.NODE_ENV === "development",
 });
-
-// ----------------------------------------------------------------------------------- //
-// Note: used for testing timestamp schema type helpers
-// const [tmp1] = await db
-// 	.insert(schema.timestamps)
-// 	.values({
-// 		text: `Init: ${Math.floor(Math.random() * 10)}`,
-// 	})
-// 	.returning();
-// console.log("tmp1: ", tmp1?.numCreatedAt?.toLocaleDateString());
-
-// await new Promise((resolve) => setTimeout(resolve, 1937));
-
-// const tmp2 = await db
-// 	.update(schema.timestamps)
-// 	.set({
-// 		text: `Mod: ${Math.floor(Math.random() * 10)}`,
-// 	})
-// 	.where(eq(schema.timestamps.id, tmp1!.id))
-// 	.returning();
-// console.log("tmp2: ", tmp2);
---
+++ packages/db-drizzle/src/query.ts
@@ -0,0 +1,46 @@
+import { eq } from "drizzle-orm";
+
+import { db } from "#db/db.ts";
+import * as schema from "#db/schema.ts";
+import * as ds from "./data-services/index.ts";
+
+// Projects
+const [p1] = await db
+	.insert(schema.projects)
+	.values({ name: "a2345678901" })
+	.returning()
+	.catch((err) => {
+		console.info(`\n.catch ERROR`); //LOG
+		console.info(`err.name: `, err.name); //LOG
+		console.info(`err.message: `, err.message); //LOG
+		console.info(`\nerr: `, err); //LOG
+		return [];
+	});
+console.info(`p1: `, p1, `\n`); //LOG
+
+try {
+	const [p2] = await ds.insertProject({ name: "b2", status: "something" });
+	console.info(`p2: `, p2, `\n`); //LOG
+} catch (err) {
+	console.info(`\ntry-catch ERROR`); //LOG
+	console.info(`\nerr: `, err); //LOG
+}
+
+// Audit
+// const [tmp1] = await db
+// 	.insert(schema.audit)
+// 	.values({
+// 		text: `Init: ${Math.floor(Math.random() * 10)}`,
+// 	})
+// 	.returning();
+// console.log("\ntmp1: ", tmp1);
+
+// await new Promise((resolve) => setTimeout(resolve, 1500));
+// const tmp2 = await db
+// 	.update(schema.audit)
+// 	.set({
+// 		text: `Mod: ${Math.floor(Math.random() * 10)}`,
+// 	})
+// 	.where(eq(schema.audit.id, tmp1.id))
+// 	.returning();
+// console.log("\ntmp2: ", tmp2);
