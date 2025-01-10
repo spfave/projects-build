e0cd26d0ad247f9703d278808acd6a2596988482
Sebastian F <sebpfdev@gmail.com>
2025-01-09 22:43:34 -0500

    Rename demos api router file

--- apps/api-hono/src/app.ts
+++ apps/api-hono/src/app.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 
-import demoApi from "#routes/demo.ts";
+import demoApi from "#routes/demos.ts";
 import projectApi from "#routes/projects.ts";
 import { notFound, onError } from "#utils/route.utils.ts";
 
