82aa5b3aeb23c729af4034f86be27e070854cc4e
Sebastian F <sebpfdev@gmail.com>
2025-01-28 23:23:32 -0500

    Slim fullstack react router stub

--- apps/fullstack-react-router/vite.config.ts
+++ apps/fullstack-react-router/vite.config.ts
@@ -5,10 +5,13 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
+	plugins: [reactRouter(), tsconfigPaths()],
+	css: {
+		postcss: {
+			plugins: [tailwindcss, autoprefixer],
+		},
+	},
+
+	server: { port: 6001 },
+	envDir: "../../",
 });
