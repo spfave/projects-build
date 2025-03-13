8d877c3d1ad1f2e65fcbf7e019430c37255ea062
Sebastian F <sebpfdev@gmail.com>
2025-03-13 18:41:11 -0400

    Demo http server

--- apps/server-go-htmx/main.go
+++ apps/server-go-htmx/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
+// ref: https://github.com/dreamsofcode-io/nethttp
 func main() {
-    fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	fmt.Println("Server starting on http://localhost:5003")
+	http.ListenAndServe(":5003", nil)
 }
