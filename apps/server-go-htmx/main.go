e813296e834ded3743072b4b96f5fb19b8663cd2
Sebastian F <sebpfdev@gmail.com>
2025-03-15 21:35:22 -0400

    Create project endpoints

--- apps/server-go-htmx/main.go
+++ apps/server-go-htmx/main.go
@@ -3,17 +3,47 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
-// ref: https://github.com/dreamsofcode-io/nethttp
+type ResponseMessage struct {
+	Msg string `json:"msg"`
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode(struct {
-			Msg string `json:"msg"`
-		}{Msg: "hello world"})
-	})
+	http.HandleFunc("GET /projects", GetAllProjects)
+	http.HandleFunc("GET /projects/{id}", GetProjectById)
+	http.HandleFunc("POST /projects", CreateProject)
+	http.HandleFunc("PUT /projects/{id}", UpdateProjectById)
+	http.HandleFunc("DELETE /projects/{id}", DeleteProject)
 
 	fmt.Println("Server starting on http://localhost:5003")
-	http.ListenAndServe(":5003", nil)
+	log.Fatal(http.ListenAndServe(":5003", nil))
+}
+
+func GetAllProjects(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(ResponseMessage{Msg: "All Projects"})
+}
+
+func GetProjectById(w http.ResponseWriter, r *http.Request) {
+	projectId := r.PathValue("id")
+	json.NewEncoder(w).Encode(ResponseMessage{Msg: "Project with Id " + projectId})
+}
+
+func CreateProject(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(ResponseMessage{Msg: "Project created"})
+}
+
+func UpdateProjectById(w http.ResponseWriter, r *http.Request) {
+	projectId := r.PathValue("id")
+	json.NewEncoder(w).Encode(ResponseMessage{
+		Msg: fmt.Sprintf("Project %s updated", projectId)})
+}
+
+func DeleteProject(w http.ResponseWriter, r *http.Request) {
+	projectId := r.PathValue("id")
+	json.NewEncoder(w).Encode(ResponseMessage{
+		Msg: fmt.Sprintf("Project %s deleted", projectId)})
 }
