3a52664be06e052c584275054d2eb0c546d5d792
Sebastian F <sebpfdev@gmail.com>
2025-03-28 22:32:17 -0400

    clean up main

--- apps/server-go-htmx/main.go
+++ apps/server-go-htmx/main.go
@@ -12,10 +12,9 @@ type ResponseMessage struct {
 }
 
 func main() {
-	// err := server(":5003").ListenAndServe()
 	apiServer := NewApiServer(5003)
 
-	fmt.Printf("P Server is running on http://localhost:%d\n", apiServer.port)
+	fmt.Printf("Server is running on http://localhost:%d\n", apiServer.port)
 	err := apiServer.Run()
 	if err != nil {
 		log.Fatal("Error in API Server execution", err)
@@ -32,13 +31,6 @@ func NewApiServer(port int) *ApiServer {
 		port: port,
 	}
 	return apiServer
-
-	// server := &http.Server{
-	// 	Addr:    fmt.Sprintf(":%d", apiServer.port),
-	// 	Handler: apiServer.RegisterHandlers(),
-	// }
-
-	// return server
 }
 
 func (apiServer *ApiServer) Run() error {
@@ -58,20 +50,6 @@ func (apiServer *ApiServer) RegisterHandlers() http.Handler {
 
 	return mux
 }
-
-// func server(addr string) *http.Server {
-// 	router := http.NewServeMux()
-// 	router.Handle("/api/v1/", http.StripPrefix("/api/v1", projectsRouter()))
-// 	router.HandleFunc("/error", handleError)
-// 	router.HandleFunc("/", handleNotFound)
-
-// 	apiServer := &http.Server{
-// 		Addr:    addr,
-// 		Handler: router,
-// 	}
-// 	fmt.Println("Server is running on http://localhost" + addr)
-// 	return apiServer
-// }
 
 // httpUtil.go
 func handleNotFound(w http.ResponseWriter, r *http.Request) {
