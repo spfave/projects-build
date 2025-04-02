9a351af8ab79fde7476b288b46e76e054aef67f3
Sebastian F <sebpfdev@gmail.com>
2025-04-01 20:54:38 -0400

    Update json encoding utilities

--- apps/server-go-htmx/pkg/web/encoding.go
+++ apps/server-go-htmx/pkg/web/encoding.go
@@ -7,17 +7,18 @@ import (
 )
 
 func JsonDecode[T any](r *http.Request) (T, error) {
+	defer r.Body.Close()
+
 	var data T
 	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
-		fmt.Print("failed to decode")
 		return data, fmt.Errorf("failed to decode from json: %w", err)
 	}
 	return data, nil
 }
 
 func JsonEncode[T any](w http.ResponseWriter, status int, data T) error {
-	w.WriteHeader(status)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		return fmt.Errorf("failed to encode to json: %w", err)
 	}
