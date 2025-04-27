b385664f504df9dba6a5ce022d195e015dbc7a64
Sebastian F <sebpfdev@gmail.com>
2025-04-26 21:06:27 -0400

    update deps

--- apps/fullstack-react-router/react-router.config.ts
+++ apps/fullstack-react-router/react-router.config.ts
@@ -7,8 +7,10 @@ export default {
 	prerender: ["/about"],
 
 	future: {
+		unstable_middleware: true,
 		unstable_optimizeDeps: true,
 		unstable_splitRouteModules: true,
+		unstable_subResourceIntegrity: true,
 		unstable_viteEnvironmentApi: true,
 	},
 } satisfies Config;
