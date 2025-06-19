acb75cd9fe37b738075141368500e0e5e4c73702
Sebastian F <sebpfdev@gmail.com>
2025-06-19 17:31:15 -0400

    Stub out projects sqlite store implementation

---
+++ apps/server-go/internal/store/projects-sqlite.go
@@ -0,0 +1,15 @@
+package store
+
+type ProjectSqliteStore struct {
+	// db sql.DB
+}
+
+// func (str *ProjectSqliteStore) GetAll() (*[]core.Project, error) {}
+
+// func (str *ProjectSqliteStore) GetById(id core.ProjectId) (*core.Project, error) {}
+
+// func (str *ProjectSqliteStore) Create(project *core.Project) (*core.Project, error) {}
+
+// func (str *ProjectSqliteStore) Update(id core.ProjectId, project *core.Project) (*core.Project, error) {}
+
+// func (str *ProjectSqliteStore) Delete(id core.ProjectId) (*core.Project, error) {}
