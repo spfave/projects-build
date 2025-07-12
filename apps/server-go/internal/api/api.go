cd55d921b0adb8ace65b3f4fcc747615ec2fed0f
Sebastian F <sebpfdev@gmail.com>
2025-07-12 19:39:53 -0400

    Define cors configuration

--- apps/server-go/internal/api/api.go
+++ apps/server-go/internal/api/api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/rs/cors"
 	pHttp "github.com/spfave/projects-build/apps/server-go/pkg/http"
 )
 
@@ -18,9 +19,18 @@ func New(port int) *ApiServer {
 }
 
 func (apiServer *ApiServer) Run() error {
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, // "http://127.0.0.1:3001"
+		AllowedMethods:   []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
+		AllowCredentials: true,
+	})
+
 	server := &http.Server{
-		Addr:    fmt.Sprintf(":%d", apiServer.Port),
-		Handler: pHttp.CorsMiddleware(apiServer.RegisterRouteHandlers()),
+		Addr: fmt.Sprintf(":%d", apiServer.Port),
+		// Handler: apiServer.RegisterRouteHandlers(), // no cors config
+		// Handler: pHttp.CorsMiddleware(apiServer.RegisterRouteHandlers()), // diy cors middleware
+		// Handler: cors.Default().Handler(apiServer.RegisterRouteHandlers()), // cors config default: GET & POST only
+		Handler: c.Handler(apiServer.RegisterRouteHandlers()), // cors config custom
 	}
 
 	return server.ListenAndServe()
