31891f90165725fc6a3e3d87787494f98356d64f
Sebastian F <sebpfdev@gmail.com>
2025-07-12 20:32:13 -0400

    Set vite server proxy with env variable

--- apps/client-react/vite.config.ts
+++ apps/client-react/vite.config.ts
@@ -1,6 +1,8 @@
 import path from "node:path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+
+const viteEnv = loadEnv("", "../../", "VITE");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +13,12 @@ export default defineConfig({
 
 	server: {
 		port: 3001,
-		proxy: { "/api": { target: "http://localhost:5003", changeOrigin: true } },
+		proxy: {
+			"/api": {
+				target: `http://localhost:${viteEnv.VITE_PROXY_PORT}`,
+				changeOrigin: true,
+			},
+		},
 	},
 	envDir: "../../",
 });
