9a483c1e9f24374dfe3e1fca5636af0341c64d05
Sebastian F <sebpfdev@gmail.com>
2026-02-17 22:35:32 -0500

    Use event listener to toggle disabling conditional fields

--- apps/client-react/src/views/project-create-route.tsx
+++ apps/client-react/src/views/project-create-route.tsx
@@ -1,12 +1,7 @@
 import * as React from "react";
 import { Link, useNavigate } from "react-router";
 
-import type {
-	ProjectErrors,
-	ProjectFields,
-	ProjectInput,
-	ProjectStatus,
-} from "@projectsbuild/core/projects";
+import type { ProjectErrors, ProjectFields } from "@projectsbuild/core/projects";
 import { transformProject, validateProject } from "@projectsbuild/core/projects";
 import { HttpResponseError } from "@projectsbuild/library/errors";
 import { formErrorsAttributes, ymdToday } from "@projectsbuild/library/utils";
@@ -20,56 +15,78 @@ import { useProjectsContext } from "./projects-route";
 
 import styles from "./project-create-route.module.css";
 
-type ProjectCreateActionState = {
-	data?: ProjectFields | null;
-	projectErrors?: ProjectErrors | null;
-	error?: string | null;
+type CreateProjectActionState = {
+	form?: ProjectFields;
+	projectErrors?: ProjectErrors;
+	error?: string;
 };
 
 export default function ProjectCreateRoute() {
-	const refForm = React.useRef<HTMLFormElement>(null);
-
 	const { fetchProjects } = useProjectsContext();
 	const navigate = useNavigate();
 	// Ref: https://www.youtube.com/watch?v=R0B2HsSM78s
-	const [state, createProjectAction, isPending] = React.useActionState<
-		ProjectCreateActionState,
-		FormData
-	>(async (_prevState, formData) => {
-		const formObj = Object.fromEntries(formData) as ProjectFields;
-		console.info(`formObj: `, formObj); // LOG
-		const validation = validateProject(formObj);
-		if (!validation.success)
-			return {
-				data: formObj,
-				error: "Invalid project",
-				projectErrors: validation.errors,
-			};
+	const [state, createProjectAction, isPending] = React.useActionState(
+		async (_prevState: CreateProjectActionState, formData: FormData) => {
+			const formObj = Object.fromEntries(formData) as ProjectFields;
+			console.info(`formObj: `, formObj); // LOG
+			const validation = validateProject(formObj);
+			if (!validation.success)
+				return {
+					form: formObj,
+					error: "Invalid project",
+					projectErrors: validation.errors,
+				};
 
-		const projectPayload = transformProject(formObj);
-		console.info(`projectPayload: `, projectPayload); // LOG
-		try {
-			const project = await client.createProject(projectPayload);
-			fetchProjects();
-			navigate(`/projects/${project.id}`);
-			return {};
-		} catch (error) {
-			if (error instanceof HttpResponseError && error.context.status === 422) {
-				// biome-ignore lint/suspicious/noExplicitAny: access <unknown> error.cause
-				const errors = (error.cause as any)?.data?.errors as ProjectErrors | undefined;
-				return { data: formObj, error: error.message, projectErrors: errors };
-			} else if (error instanceof Error) {
-				return { data: formObj, error: error.message };
-			} else throw error;
+			const projectPayload = transformProject(formObj);
+			console.info(`projectPayload: `, projectPayload); // LOG
+			try {
+				const project = await client.createProject(projectPayload);
+				fetchProjects();
+				navigate(`/projects/${project.id}`);
+				return {};
+			} catch (error) {
+				if (error instanceof HttpResponseError && error.context.status === 422) {
+					// biome-ignore lint/suspicious/noExplicitAny: access <unknown> error.cause
+					const errors = (error.cause as any)?.data?.errors as ProjectErrors | undefined;
+					return { form: formObj, error: error.message, projectErrors: errors };
+				} else if (error instanceof Error) {
+					return { form: formObj, error: error.message };
+				} else throw error;
+			}
+		},
+		{}
+	);
+
+	const refForm = React.useRef<HTMLFormElement>(null);
+	const refStatus = React.useRef<HTMLSelectElement>(null);
+	// Note: Adds event listener that toggles disabling conditional fields so that they are only captured in the FormData when relevant (status = "complete"). Otherwise they are captured as empty string values and considered invalid by project validation function.
+	React.useEffect(() => {
+		const form = refForm.current;
+		const status = refStatus.current;
+		if (!form || !status) return;
+
+		function toggleConditionalFields() {
+			const isComplete = status?.value === "complete";
+			form
+				?.querySelectorAll<HTMLInputElement>(
+					`.${styles.conditional} > input, .${styles.conditional} > fieldset`
+				)
+				.forEach((input) => {
+					input.disabled = !isComplete;
+				});
 		}
-	}, {});
+		status.addEventListener("change", toggleConditionalFields);
+		toggleConditionalFields();
+
+		return () => status.removeEventListener("change", toggleConditionalFields);
+	}, [state.form?.status]); // Note: need unnecessary dependency to rerun effect with changing key on select otherwise event listener is lost with remount
 
 	const rerender = useRerender();
 	function handleReset(_evt: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
 		refForm.current?.reset(); // ?? needed
-		state.data = null; // Note: hack to reset useActionState state variable: clears defaultValues on inputs, top-level error message, and input field errors
-		state.error = null;
-		state.projectErrors = null;
+		state.form = undefined; // Note: hack to reset useActionState state variable: clears defaultValues on inputs, top-level error message, and input field errors
+		state.error = undefined;
+		state.projectErrors = undefined;
 		rerender(); // Note: needed in case project status is unset
 	}
 
@@ -104,7 +121,7 @@ export default function ProjectCreateRoute() {
 							placeholder="project title"
 							required
 							minLength={2}
-							defaultValue={state.data?.name ?? ""}
+							defaultValue={state.form?.name ?? ""}
 							aria-invalid={errAttrFields?.name.hasErrors}
 							aria-describedby={errAttrFields?.name.id}
 						/>
@@ -119,7 +136,7 @@ export default function ProjectCreateRoute() {
 							type="url"
 							name="link"
 							placeholder="image or site url"
-							defaultValue={state.data?.link ?? ""}
+							defaultValue={state.form?.link ?? ""}
 							aria-invalid={errAttrFields?.link.hasErrors}
 							aria-describedby={errAttrFields?.link.id}
 						/>
@@ -134,7 +151,7 @@ export default function ProjectCreateRoute() {
 						id="description"
 						name="description"
 						placeholder="add description..."
-						defaultValue={state.data?.description ?? ""}
+						defaultValue={state.form?.description ?? ""}
 						aria-invalid={errAttrFields?.description.hasErrors}
 						aria-describedby={errAttrFields?.description.id}
 					/>
@@ -151,7 +168,7 @@ export default function ProjectCreateRoute() {
 						id="notes"
 						name="notes"
 						placeholder="add notes..."
-						defaultValue={state.data?.notes ?? ""}
+						defaultValue={state.form?.notes ?? ""}
 						aria-invalid={errAttrFields?.notes.hasErrors}
 						aria-describedby={errAttrFields?.notes.id}
 					/>
@@ -163,11 +180,12 @@ export default function ProjectCreateRoute() {
 					<div>
 						<label htmlFor="status">Status</label>
 						<select
-							key={state.data?.status} // note: forces remount so defaultValue updates when status changes, uncontrolled select ignores new defaultValue otherwise
+							ref={refStatus}
+							key={state.form?.status} // Note: forces remount so defaultValue updates when status changes, uncontrolled select ignores new defaultValue otherwise
 							id="status"
 							name="status"
 							required
-							defaultValue={state.data?.status ?? ""} // note: does not work for controlled select
+							defaultValue={state.form?.status ?? ""} // Note: works with uncontrolled select only on initial mount
 							aria-invalid={errAttrFields?.status.hasErrors}
 							aria-describedby={errAttrFields?.status.id}
 						>
@@ -191,7 +209,7 @@ export default function ProjectCreateRoute() {
 							required
 							max={ymdToday()}
 							defaultValue={
-								state.data?.status === "complete" ? state.data?.dateCompleted : undefined
+								state.form?.status === "complete" ? state.form?.dateCompleted : undefined
 							}
 							aria-invalid={errAttrFields?.dateCompleted.hasErrors}
 							aria-describedby={errAttrFields?.dateCompleted.id}
@@ -216,7 +234,7 @@ export default function ProjectCreateRoute() {
 							min={1}
 							max={5}
 							// @ts-expect-error: optional chaining check over type narrowing
-							defaultValue={state.data?.rating ?? ""}
+							defaultValue={state.form?.rating ?? ""}
 							aria-invalid={errAttrFields?.rating.hasErrors}
 							aria-describedby={errAttrFields?.rating.id}
 						/>
@@ -239,7 +257,7 @@ export default function ProjectCreateRoute() {
 										required
 										value="true"
 										// @ts-expect-error: optional chaining check over type narrowing
-										defaultChecked={state.data?.recommend === "true"}
+										defaultChecked={state.form?.recommend === "true"}
 									/>
 									<label htmlFor="recommend-yes">Yes</label>
 								</span>
@@ -251,7 +269,7 @@ export default function ProjectCreateRoute() {
 										required
 										value="false"
 										// @ts-expect-error: optional chaining check over type narrowing
-										defaultChecked={state.data?.recommend === "false"}
+										defaultChecked={state.form?.recommend === "false"}
 									/>
 									<label htmlFor="recommend-no">No</label>
 								</span>
