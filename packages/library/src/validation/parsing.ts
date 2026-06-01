204c5a1b1eaac07c506ce381b6b2ea94160db234
Sebastian F <sebpfdev@gmail.com>
2026-06-01 15:42:29 -0400

    Demo jsdoc type definitions

--- packages/library/src/validation/parsing.ts
+++ packages/library/src/validation/parsing.ts
@@ -1,7 +1,7 @@
 /**
  * Evaluates if a value of `unknown` type can be parsed to an integer.
- * @param value
- * @returns
+ * @param {unknown} value
+ * @returns {boolean}
  */
 export function isParsableInt(value: unknown): boolean {
 	if (typeof value === "number") return Number.isSafeInteger(value);
@@ -11,8 +11,8 @@ export function isParsableInt(value: unknown): boolean {
 
 /**
  * Evaluates if a `string` can be parsed to an integer.
- * @param value
- * @returns
+ * @param {string} value
+ * @returns {boolean}
  */
 export function isStringParsableInt(value: string): boolean {
 	return Number(value) === Number.parseInt(value, 10);
