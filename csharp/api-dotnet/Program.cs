6891d9813e87493e2fffd08ac394f76c93f7e9f7
Sebastian Favè <sebpfdev@gmail.com>
2026-07-05 20:07:59 +0000

    Demo minimal endpoints

--- csharp/api-dotnet/Program.cs
+++ csharp/api-dotnet/Program.cs
@@ -2,5 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/projects", () => "get projects");
+app.MapGet("/projects/{id}", (string id) => $"get project {id}");
+app.MapPost("/projects", () => "create project");
+app.MapPut("/projects/{id}", (string id) => $"updated project {id}");
+
+
+
+app.MapDelete("/projects/{id}", (string id) => $"delete project {id}");
 
 app.Run();
