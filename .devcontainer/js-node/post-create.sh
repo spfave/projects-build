1f7b7b2b96ba5381b94ea1c823e20f361839fbcf
Sebastian F <sebpfdev@gmail.com>
2026-07-04 21:52:45 -0400

    Move demo .Net api to new organization structure

--- .devcontainer/js-node/post-create.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# chmod +x $0
-set -euo pipefail
-echo "DEV CONTAINER: TYPESCRIP kPOST CREATE SCRIPT"
-
-# PNPM config
-export PATH="$PNPM_HOME/bin:$PATH"
-pnpm config set -g store-dir "$PNPM_HOME/store"
-pnpm config set -g global-bin-dir "$PNPM_HOME/bin"
