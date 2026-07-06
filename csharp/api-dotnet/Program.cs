b02e829868c05594698a56cb52cef640b8f19c42
Sebastian Favè <sebpfdev@gmail.com>
2026-07-06 20:29:00 +0000

    Add scalar endpoint browse/test ui

--- csharp/api-dotnet/Program.cs
+++ csharp/api-dotnet/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
@@ -5,6 +7,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
 	app.MapOpenApi();
+	app.MapScalarApiReference();
 }
 
 app.MapGet("/", () => "Hello World!");
