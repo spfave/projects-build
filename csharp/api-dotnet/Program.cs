28e091460baeb8c98ca1ae01371f28ebe4e6c3f1
Sebastian Favè <sebpfdev@gmail.com>
2026-07-06 22:16:10 +0000

    Demo adding openapi metadata

--- csharp/api-dotnet/Program.cs
+++ csharp/api-dotnet/Program.cs
@@ -1,18 +1,40 @@
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi("projects-build", (options) =>
+{
+	options.AddDocumentTransformer((document, context, ct) =>
+	{
+		document.Info.Title = "Projects.build | api-dotnet";
+		document.Info.Summary = "API to manage project builds";
+		document.Info.Description = "JSON API to manage and track project builds. Built with C# and ASP.NET";
+		return Task.CompletedTask;
+	});
+});
+
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
 	app.MapOpenApi();
-	app.MapScalarApiReference();
+	app.MapScalarApiReference("/openapi/scalar/");
 }
 
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/projects", () => "get projects");
-app.MapGet("/projects/{id}", (string id) => $"get project {id}");
+
+app.MapGet("/projects", () => "get projects")
+	.WithName("Get Projects")
+	.WithDisplayName("Get Projects list")
+	.WithSummary("Get Projects list")
+	.WithDescription("Returns JSON list of Projects. Includes Project Id and Name only")
+	.WithTags("Projects");
+app.MapGet("/projects/{id}",
+[EndpointName("Get Project")]
+[EndpointSummary("Get Project by Id")]
+[EndpointDescription("Returns JSON representation of entire Project if found by Project Id")]
+[Tags("Projects")]
+(string id) => $"get project {id}"
+);
 app.MapPost("/projects", () => "create project");
 app.MapPut("/projects/{id}", (string id) => $"updated project {id}");
 
