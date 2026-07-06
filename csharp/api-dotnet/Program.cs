a87d8f30f7769981ef3abeb10dc8325439488735
Sebastian Favè <sebpfdev@gmail.com>
2026-07-06 20:26:47 +0000

    Add openapi doc generation

--- csharp/api-dotnet/Program.cs
+++ csharp/api-dotnet/Program.cs
@@ -1,5 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+	app.MapOpenApi();
+}
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/projects", () => "get projects");
