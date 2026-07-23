524cdde8e8605f5afe55bbc5f2bb17a8b0e0ff33
Sebastian Favè <sebpfdev@gmail.com>
2026-07-23 01:16:09 +0000

    Add openapi details to project routes

--- csharp/api-dotnet/Routes/ProjectRouter.cs
+++ csharp/api-dotnet/Routes/ProjectRouter.cs
@@ -4,12 +4,12 @@ internal static class ProjectRouter
 {
 	internal static void MapProjectRoutes(this IEndpointRouteBuilder router)
 	{
-		var projectRouter = router.MapGroup("/api/v1").WithTags("Projects");
+		var projectRouter = router.MapGroup("/api/v1/projects").WithTags("Projects");
 
 		projectRouter
 			.MapGet(
-				"/projects",
-				() =>
+				"/",
+				async () =>
 				{
 					return TypedResults.Ok(new { Message = "get projects" });
 				}
@@ -19,29 +19,45 @@ internal static class ProjectRouter
 			.WithDescription("Returns JSON list of Projects. Includes Project Id and Name only");
 
 		projectRouter.MapGet(
-			"/projects/{id}",
+			"/{id}",
 			[EndpointName("Get Project")]
 			[EndpointSummary("Get Project by Id")]
-			[EndpointDescription("Returns JSON representation of entire Project if found by Project Id")]
-			(string id) => TypedResults.Ok(new { Message = $"get project {id}" })
+			[EndpointDescription("Returns JSON representation of Project if found by Project Id")]
+			async (string id) => TypedResults.Ok(new { Message = $"get project {id}" })
 		);
 
-		projectRouter.MapPost(
-			"/projects",
-			() =>
-			{
-				return TypedResults.Created("/projects/<ProjectId>", new { Message = "create project" });
-			}
-		);
+		projectRouter
+			.MapPost(
+				"/",
+				async () =>
+				{
+					return TypedResults.Created("/<ProjectId>", new { Message = "create project" });
+				}
+			)
+			.WithName("Create Project")
+			.WithSummary("Create Project")
+			.WithDescription("Creates a new Project. Returns JSON representation of created Project");
 
-		projectRouter.MapPut(
-			"/projects/{id}",
-			(string id) => TypedResults.Ok(new { Message = $"updated project {id}" })
-		);
+		projectRouter
+			.MapPut(
+				"/{id}",
+				async (string id) => TypedResults.Ok(new { Message = $"updated project {id}" })
+			)
+			.WithName("Update Project")
+			.WithSummary("Update Project")
+			.WithDescription(
+				"Updates Project if found by Id. Returns JSON representation of updated Project otherwise not-found"
+			);
 
-		projectRouter.MapDelete(
-			"/projects/{id}",
-			(string id) => TypedResults.Ok(new { Message = $"delete project {id}" })
-		);
+		projectRouter
+			.MapDelete(
+				"/{id}",
+				async (string id) => TypedResults.Ok(new { Message = $"delete project {id}" })
+			)
+			.WithName("Delete Project")
+			.WithSummary("Delete Project")
+			.WithDescription(
+				"Deletes Project if found by Id. Returns JSON representation of deleted Project otherwise not-found"
+			);
 	}
 }
