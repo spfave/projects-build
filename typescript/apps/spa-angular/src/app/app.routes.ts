4387da0eda38116befc535e03f7bc3cf1187d6df
Sebastian F <sebpfdev@gmail.com>
2026-08-02 17:47:26 -0400

    Add catch all route to display page not found

--- typescript/apps/spa-angular/src/app/app.routes.ts
+++ typescript/apps/spa-angular/src/app/app.routes.ts
@@ -1,8 +1,17 @@
+import { Component } from "@angular/core";
 import type { Routes } from "@angular/router";
 
 import { AboutPage } from "~/views/about-page";
 import { ProjectsLayout } from "~/views/projects-layout";
 import { Root } from "~/views/root";
+
+@Component({
+	selector: "pb-not-found-page",
+	template: `
+		<div>Page Not Found</div>
+	`,
+})
+class NotFoundPage {}
 
 export const routes: Routes = [
 	{
@@ -12,6 +21,7 @@ export const routes: Routes = [
 			{ path: "", redirectTo: "projects", pathMatch: "full" },
 			{ path: "projects", component: ProjectsLayout, children: [] },
 			{ path: "about", title: "About | Projects.build", component: AboutPage },
+			{ path: "**", component: NotFoundPage },
 		],
 	},
 ];
