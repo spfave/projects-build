b1dcf001ad134690a2bc0a103da6e265ff0c3fdb
Sebastian F <sebpfdev@gmail.com>
2026-08-15 18:55:35 -0400

    Explore async utilities for tracking async state on observables

---
+++ typescript/apps/spa-angular/src/shared/async-state.ts
@@ -0,0 +1,87 @@
+import type { Observable, OperatorFunction } from "rxjs";
+import { BehaviorSubject, catchError, defer, map, of, startWith, tap } from "rxjs";
+
+// Ref: https://angular.dev/guide/signals/resource#resource-status
+export type AsyncState<TValue = unknown> =
+	| { status: "idle"; value: null; error: null }
+	| { status: "loading"; value: null; error: null }
+	| { status: "resolved"; value: TValue; error: null }
+	| { status: "error"; value: TValue | null; error: unknown };
+export type AsyncStatus = AsyncState["status"];
+export const asyncInitialState: AsyncState = {
+	status: "loading",
+	value: null,
+	error: null,
+};
+
+// ----------------------------------------------------------------------------------- //
+// #region - Async Observable to Signal Utilities
+// export function toAsyncState<T>(queryFn: () => Observable<T>) {}
+// export function toQuerySignal<T>(queryFn: () => Observable<T>) {}
+// export function toMutationSignal(){}
+
+// #endregion
+
+// ----------------------------------------------------------------------------------- //
+// #region - Async Ovservable Utilities
+// Distinct tracking for observable and associated async metadata
+export type AsyncOperation<TValue = unknown> = {
+	value$: Observable<TValue | null>;
+	state$: Observable<AsyncState<TValue>>;
+};
+
+export function trackAsync<T>(source$: Observable<T>): AsyncOperation<T> {
+	const _state = new BehaviorSubject<AsyncState<T>>(asyncInitialState as AsyncState<T>);
+
+	const _value$ = defer(() => {
+		_state.next({ status: "loading", value: null, error: null });
+		return source$.pipe(
+			tap({
+				next: (value) => _state.next({ status: "resolved", value, error: null }),
+				error: (error: unknown) =>
+					_state.next({ status: "error", value: _state.value.value, error }),
+			})
+		);
+	});
+
+	return {
+		value$: _value$,
+		state$: _state.asObservable(),
+	};
+}
+
+// type MapObservable<T extends RecordGen> = {
+// 	[K in keyof T as `${K & string}$`]: Observable<T[K]>;
+// };
+// export type AsyncState$<TValue = unknown> = MapObservable<AsyncState<TValue>>;
+
+// Unified tracking for observable async state with metadata
+export function asyncStateThrow<T>(): OperatorFunction<T, AsyncState<T>> {
+	return (source$: Observable<T>): Observable<AsyncState<T>> => {
+		return source$.pipe(
+			map((value) => ({ status: "resolved", value, error: null }) as const),
+			catchError((error: unknown) => {
+				throw error;
+			}),
+			startWith(asyncInitialState as AsyncState<T>)
+		);
+	};
+}
+
+export function asyncWrapError<T>(): OperatorFunction<AsyncState<T>, AsyncState<T>> {
+	return (source$: Observable<AsyncState<T>>): Observable<AsyncState<T>> => {
+		return source$.pipe(
+			catchError((error: unknown) =>
+				of({ status: "error", value: null, error } as AsyncState<T>)
+			)
+		);
+	};
+}
+
+export function asyncState<T>(): OperatorFunction<T, AsyncState<T>> {
+	return (source$: Observable<T>): Observable<AsyncState<T>> => {
+		return source$.pipe(asyncStateThrow(), asyncWrapError());
+	};
+}
+
+// #endregion
--- typescript/apps/spa-angular/src/views/project-loading-page.ts
+++ typescript/apps/spa-angular/src/views/project-loading-page.ts
@@ -3,16 +3,18 @@ import { HttpClient, type HttpErrorResponse, httpResource } from "@angular/commo
 import { Component, inject, input, type OnInit, resource } from "@angular/core";
 import { toSignal } from "@angular/core/rxjs-interop";
 import { ActivatedRoute } from "@angular/router";
-import { catchError, map, switchMap, tap, throwError } from "rxjs";
+import { catchError, delay, map, of, switchMap, tap } from "rxjs";
 
 import type { Project } from "@projectsbuild/core/project";
 import { environment as ENV } from "~/environments/environment";
+import { asyncInitialState, asyncState, trackAsync } from "~/shared/async-state";
 
 @Component({
 	selector: "pb-project-page",
 	imports: [AsyncPipe, JsonPipe],
 	template: `
 		<section>
+			<!-- Signal Resources -->
 			<div>
 				<h2>Project Resource signal</h2>
 				@if (projRs.hasValue()) {
@@ -41,17 +43,58 @@ import { environment as ENV } from "~/environments/environment";
 					<p>err: {{ projHRs.error() | json }}</p>
 				}
 			</div>
+
+			<!-- Observables / Subscriptions -->
 			<div>
 				<h2>Project Observable</h2>
 				<p>val: {{ projOb$ | async | json }}</p>
 			</div>
 			<div>
-				<h2>Project toSignal(Observable)</h2>
-				<p>val: {{ projObS() | json }}</p>
-			</div>
-			<div>
 				<h2>Project Observable.Subscription()</h2>
 				<p>val: {{ projSub | json }}</p>
+			</div>
+			<div>
+				<h2>Project Observable |> Async</h2>
+				@if (projObAsync$ | async; as projObAsync) {
+					@switch (projObAsync.status) {
+						@case ("resolved") {
+							<p>val: {{ projObAsync.value | json }}</p>
+						}
+						@case ("loading") {
+							<p>tmp: Loading Projects...</p>
+						}
+						@case ("error") {
+							<p>Failed to load project</p>
+							<p>err.error: {{ $any(projObAsync.error).error | json}}</p>
+							<p>err: {{ projObAsync.error | json}}</p>
+						}
+					}
+				}
+			</div>
+			<div>
+				<h2>track(Project Observable)</h2>
+				<!-- @if (projObTrackState| async; as trkProjOb) { -->
+				@if (projObTrackState; as trkProjOb) {
+					@switch (trkProjOb.status) {
+						@case ("resolved") {
+							<p>val: {{ trkProjOb.value | json }}</p>
+						}
+						@case ("loading") {
+							<p>tmp: Loading Projects...</p>
+						}
+						@case ("error") {
+							<p>Failed to load project</p>
+							<p>err.error: {{ $any(trkProjOb.error).error | json}}</p>
+							<p>err: {{ trkProjOb.error | json}}</p>
+						}
+					}
+				}
+			</div>
+
+			<!-- Transforms -->
+			<div>
+				<h2>Project toSignal(Observable)</h2>
+				<p>val: {{ projObS() | json }}</p>
 			</div>
 		</section>
 	`,
@@ -89,16 +132,22 @@ export class ProjectLoadingPage implements OnInit {
 			return js as Project;
 		},
 	});
-	// protected readonly projHRs = httpResource<Project>(() => `${this.urlApi}?d=hrs`);
+	// protected readonly _projHRs = httpResource<Project>(
+	// 	() => `${this.urlApi}/${this.projIdParam()}?d=hrs`
+	// );
 	protected readonly projHRs = httpResource<Project>(() => ({
 		url: `${this.urlApi}/${this.projIdParam()}`, // note: api error (status 4xx/5xx) provided through .error() result
 		params: { d: "hrs" },
 	}));
-	protected projSub?: Project | null = null;
+
 	protected _projOb$ = this.http
-		.get<Project>(`${this.urlApi}/${this.projIdSs}?d=ob$`)
-		.pipe(tap((proj) => console.info(`tap: projOb$: `, proj)));
+		.get<Project>(`${this.urlApi}/${this.projIdSs}?d=_ob$`)
+		.pipe(
+			delay(1000),
+			tap((proj) => console.info(`tap: projOb$: `, proj))
+		);
 	protected projOb$ = this.projId$.pipe(
+		delay(1000),
 		switchMap((projId) =>
 			this.http.get<Project>(`${this.urlApi}/${projId}?d=ob$`, { observe: "response" })
 		),
@@ -113,6 +162,13 @@ export class ProjectLoadingPage implements OnInit {
 			// return throwError(() => err);
 		})
 	);
+	protected projSub?: Project | null = null;
+
+	private readonly projObTrackAsync = trackAsync(this._projOb$);
+	// protected projObTrackState = of(asyncInitialState);
+	protected projObTrackState = asyncInitialState;
+	readonly projObAsync$ = this._projOb$.pipe(asyncState());
+
 	protected projObS = toSignal(this.projOb$);
 
 	constructor() {
@@ -135,17 +191,23 @@ export class ProjectLoadingPage implements OnInit {
 			},
 			complete: () => console.info("Proj subscription complete"),
 		});
-		this.projRs.reload();
+
+		this.projObTrackAsync.value$.subscribe();
+		// this.projObTrackState = this.projObTrackAsync.state$;
+		this.projObTrackAsync.state$.subscribe((state) => (this.projObTrackState = state));
 	}
 
 	private _typeExploring() {
 		const _idP = this.projIdParam();
 
-		const _sub = this.projSub;
-		const _ob$ = this.projOb$;
-		const _sig = this.projObS;
-		const _rs = this.projRs;
-		const _hrs = this.projHRs;
+		const _pRs = this.projRs;
+		const _pHrs = this.projHRs;
+		const _pSub = this.projSub;
+		const _pOb$ = this.projOb$;
+		const _pSig = this.projObS;
+
+		const _pTr$ = this.projObTrackAsync;
+		const _pAy$ = this.projObAsync$;
 	}
 }
 
